312a7c674bedaf4ff583d89dece9ec90f190e36a
neketkaaaaagit <167111881+neketkaaaaagit@users.noreply.github.com>
2025-01-21 21:29:49 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+// vite-env.d.ts или global.d.ts
+declare global {
+  interface Window {
+    ym: any; // Указываем тип `any`, так как точный тип будет зависеть от данных, которые метрика передает.
+  }
+}
+
+export {};
